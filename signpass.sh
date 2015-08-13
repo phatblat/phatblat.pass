1deb6ebd32d293febb8595d9ceeaaef397720b5a
Ben Chatelain <benchatelain@gmail.com>
2015-08-13 11:02:25 -0600

    Sign the pass

--- signpass.sh
+++ signpass.sh
@@ -6,8 +6,11 @@
 #
 
 cmd="signpass/bin/signpass"
+pass="Generic.pass"
 
-if [ ! -f $cmd ]; then
-    echo "$cmd not found! Please build the xignpass.xcodeproj first, then re-run this script."
+if [ ! -f ${cmd} ]; then
+    echo "${cmd} not found! Please build the xignpass.xcodeproj first, then re-run this script."
     exit 1
 fi
+
+"${cmd}" -p "${pass}"
